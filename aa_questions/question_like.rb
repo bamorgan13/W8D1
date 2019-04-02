2cc41c104ee4b26a23894583d408740cb0f8ecf3
Bryce Morgan <bamorgan13@gmail.com>
2019-04-01 21:53:33 -0400

    Add QuestionLike::likers_for_question_id, QuestionLike::num_likes_for_question_id, and QuestionLike::liked_questions_for_user_id

--- aa_questions/question_like.rb
+++ aa_questions/question_like.rb
@@ -18,6 +18,55 @@ class QuestionLike
         QuestionLike.new(data.first)
     end
 
+    def self.likers_for_question_id(question_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT
+                users.*
+            FROM
+                users
+            JOIN
+                question_likes
+            ON
+                users.id = question_likes.user_id
+            WHERE
+                question_likes.question_id = ?
+        SQL
+
+        data.map {|datum| User.new(datum)}
+    end
+
+    def self.num_likes_for_question_id(question_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT
+                COUNT(*) AS num_likes
+            FROM
+                questions
+            JOIN
+                question_likes
+            ON
+                questions.id = question_likes.question_id
+            WHERE
+                questions.id = ?
+        SQL
+    end
+
+    def self.liked_questions_for_user_id(user_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+            SELECT
+                questions.*
+            FROM
+                questions
+            JOIN
+                question_likes
+            ON
+                questions.id = question_likes.question_id
+            WHERE
+                question_likes.user_id = ?
+        SQL
+
+        data.map {|datum| Question.new(datum)}
+    end
+
     attr_accessor :user_id, :question_id
     attr_reader :id
 
